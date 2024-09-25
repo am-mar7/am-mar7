170510b5208a4ff3e74c7b6442ec7f44e9471a61
am-mar7 <146647513+am-mar7@users.noreply.github.com>
2024-09-25 19:55:51 +0300

    Update BigInteger.cpp

--- BigInteger.cpp
+++ BigInteger.cpp
@@ -68,5 +68,52 @@ struct BigInteger
         if(ret.empty()) return BigInteger("0");
         return BigInteger (ret);
     }
-    
+    bool issmaller (string n1, string n2){
+        if(n1.length()<n2.length()) 
+            return 1;
+        if(n1.length() > n2.length()) 
+            return 0;
+        for(int i = 0; i<n1.length(); i++)
+            if(n1[i] < n2[i]) 
+                return 1;
+            else if(n1[i] > n2[i])
+                return 0;
+        return 0;        
+    }
+    BigInteger operator- (const BigInteger b){
+        string a = str, c = b.str;
+        if(a == c)return BigInteger("0");
+        bool isnega = 0;
+        if(issmaller(a,c)){
+            swap(a,c);
+            isnega = 1;
+        }
+        reverse(a.begin(),a.end());
+        reverse(c.begin(),c.end());
+        string res = "";
+        int carry= 0;
+        for(int i =0 ; i < c.size() ; i++){
+            int diff = ((a[i]-'0') - (c[i] - '0')) -carry;
+            if(diff < 0){
+                diff += 10;
+                carry =1;
+            }
+            else carry =0;
+            res.push_back(diff+'0');
+        }
+        for(int i = c.size(); i < a.size(); i++){
+            int diff = (a[i]-'0') - carry;
+            if(diff<0){
+                diff += 10;
+                carry =1;
+            }
+            else carry= 0;
+            res.push_back(diff+'0'); 
+        }
+        while(res.back() == '0')res.pop_back();
+        if(isnega)
+            res.push_back('-');
+        reverse(res.begin(),res.end());
+        return BigInteger(res);
+    }    
 };
