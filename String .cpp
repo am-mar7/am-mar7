8132b0d15f9228365e3899cd3208f07a04c81a24
am-mar7 <146647513+am-mar7@users.noreply.github.com>
2024-10-28 13:13:57 +0300

    Create String .cpp

---
+++ String .cpp
@@ -0,0 +1,23 @@
+//***********************Nah I would win*************************//
+int to_int(string s){//2736548
+    int x = 0;
+    For(s.length()){
+        int dig = s[i] - '0';
+        x*=10;
+        x+= dig; 
+    }
+    return x;
+}
+//***********************Nah I would win*************************//
+bool is_a_sub_string (string sub , string x){
+    if(sub.length() > x.length())return 0;
+    if(sub == x)return 1;
+    int i = 0 ,j =0;
+    while(j < x.length()){
+        if(i == sub.length())return 1;
+        if(sub[i] == x[j])i++;
+        else i = (sub[0] == x[j]);
+        j++;
+    }
+    return 0;
+}
