85027ae1bbe6954269881136bb6accdd3f84b7d0
am-mar7 <146647513+am-mar7@users.noreply.github.com>
2025-02-12 16:33:58 +0200

    Update Number Theroy.cpp

--- Number Theroy.cpp
+++ Number Theroy.cpp
@@ -180,3 +180,21 @@ vector<int> primesToN (int n){ // sieve approch
     }   
     return ret;
 } 
+/***************************************/
+int extended_euclid(int a , int b , int& x , int& y){
+    if(b == 0){
+        x =  1, y = 0;
+        return a;
+    }
+    int g =  extended_euclid(b , a%b , y , x);
+    y -=  (a / b) * x;
+    return g;
+}
+/***************************************/
+int modInverse (int a , int m){
+    int x, y;
+    int g = extended_euclid(a , m , x, y) ;
+    if(g == 1)
+        return (x + m)%m;
+    return -1;    
+}
